6ff7974fc717a5112c15a47774543f0261062d0e
Abhijith Antony <43300913+abhi285@users.noreply.github.com>
2025-04-30 01:15:11 -0400

    Create template_full_refresh.sql

---
+++ template_full_refresh.sql
@@ -0,0 +1,12 @@
+{% set src_table = var('full_load_src_table') %}
+{% set tgt_table = var('full_load_tgt_table') %}
+ 
+{{ config(
+    materialized='table',
+    alias=tgt_table,
+    post_hook=["{{ apply_dynamic_masking()}}"]
+) }}
+ 
+SELECT
+        {{get_columns(src_table)}}
+FROM {{ ref(src_table) }}
